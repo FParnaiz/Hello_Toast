7a5f55170c200ae9fd371a6c519a2d72523526a2
FParnaiz <147650301+FParnaiz@users.noreply.github.com>
2024-11-05 16:11:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.hellotoast;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView contador;
+    Button  bToast;
+    Button  bCount;
+    int count=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        bToast=findViewById(R.id.button_toast);
+        bCount=findViewById(R.id.button_count);
+        contador=findViewById(R.id.show_count);
+        bCount.setOnClickListener(v -> {
+
+                    count++;
+            contador.setText(String.valueOf(count));
+        });
+        bToast.setOnClickListener(v -> {
+            Toast.makeText(this, "Has contado hasta: "+count, Toast.LENGTH_SHORT).show();
+        });
+        }
+
+    }
